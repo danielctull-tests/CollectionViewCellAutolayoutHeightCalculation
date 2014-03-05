5b2979e70e9a43bfcfc6517b3d69018402ee05e8
Daniel Tull <dt@danieltull.co.uk>
2014-03-05 09:01:19 +0000

    Initial Commit

---
+++ CollectionViewCellAutolayoutHeightCalculation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CollectionViewCellAutolayoutHeightCalculation
+//
+//  Created by Daniel Tull on 05.03.2014.
+//  Copyright (c) 2014 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
